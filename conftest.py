f67b51a55ac74a7ea86a97a7098bc584b96e6015
Pranjal Melkani <pranjal.melkani18@gmail.com>
2025-04-01 15:05:47 +0530

    driver initalization and teardown

---
+++ conftest.py
@@ -0,0 +1,19 @@
+import pytest
+from selenium import webdriver
+
+@pytest.fixture(scope='session', autouse=True, params=['chrome'])
+def driver(request):
+    browser = request.param
+    
+    if browser == 'chrome':
+        driver = webdriver.Chrome()
+    elif browser == 'edge':
+        driver = webdriver.Edge()
+    elif browser == 'firefox':
+        driver = webdriver.Firefox()
+    else:
+        raise Exception("Please check the browser type")
+    
+    driver.maximize_window()
+    yield driver
+    driver.quit()
