c6370a2789563fcd6066259f4cc9b8fda3d3dde3
Pranjal Melkani <pranjal.melkani18@gmail.com>
2025-04-02 14:53:25 +0530

    updated locators

--- utilities/locators.py
+++ utilities/locators.py
@@ -2,22 +2,19 @@ from selenium.webdriver.common.by import By
 
 
 class Homepage_locators:
-    signup_login_btn = (By.XPATH, "//a[contains(text(), 'Signup / Login')]")
-    contact_us_btn = (By.XPATH, "//a[contains(text(), 'Contact us')]")
-    test_cases_btn = (By.XPATH, "//a[contains(text(), 'Test Cases')]")
-    products_btn = (By.XPATH, "//a[contains(text(), 'Products')]")
-    cart_btn = (By.XPATH, "//a[contains(text(), 'Cart')]")
-    view_product = (By.LINK_TEXT, "View Product")
-    add_to_cart = (By.LINK_TEXT, "Add to cart")
+    # view_product = (By.LINK_TEXT, "View Product")
+    # add_to_cart = (By.LINK_TEXT, "Add to cart")
+    pass
     
+
 class Signup_Login_page_locators:
+    # Signup
     signup_form = (By.CLASS_NAME, "signup-form")
     signup_header = (By.XPATH, "//*[text()='New User Signup!']")
     signup_name_field = (By.XPATH, "//*[@data-qa='signup-name']")
     signup_email_field = (By.XPATH, "//*[@data-qa='signup-email']")
     signup_btn = (By.XPATH, "//*[@data-qa='signup-button']")
     
-    # Signup page
     enter_account_info_header = (By.XPATH, "//*[text()='Enter Account Information']")
     title_mr = (By.ID, "id_gender1")
     title_mrs = (By.ID, "id_gender2")
@@ -38,8 +35,17 @@ class Signup_Login_page_locators:
     zipcode_field = (By.ID, "zipcode")
     mobile_no_field = (By.ID, "mobile_number")
     create_account_btn = (By.XPATH, "//*[@data-qa='create-account']")
+    
     account_created_message = (By.XPATH, "//*[text()='Account Created!']")
     continue_btn = (By.XPATH, "//*[@data-qa='continue-button']")
-    logged_in_as_field = (By.CSS_SELECTOR, ".navbar-nav li:last-child")
     delete_account_btn = (By.XPATH, "//*[contains(text(), 'Delete Account')]")
     account_deleted_message = (By.XPATH, "//*[text()='Account Deleted!']")
+    email_exists_error_msg = (By.XPATH, "//*[text()='Email Address already exist!']")
+    
+    # Login
+    login_form = (By.CLASS_NAME, "login-form")
+    login_header = (By.XPATH, "//*[text()='Login to your account']")
+    login_email_field = (By.XPATH, "//*[@data-qa='login-email']")
+    login_password_field = (By.XPATH, "//*[@data-qa='login-password']")
+    login_btn = (By.XPATH, "//*[@data-qa='login-button']")
+    incorrect_credentials_error_msg = (By.XPATH, "//*[text()='Your email or password is incorrect!']")
