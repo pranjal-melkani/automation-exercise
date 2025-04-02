b11603ee80ca1573398f6e551ba8ab679fe1d6ae
Pranjal Melkani <pranjal.melkani18@gmail.com>
2025-04-02 16:51:53 +0530

    homepage updated wrt contact us

--- pages/homepage.py
+++ pages/homepage.py
@@ -1,6 +1,7 @@
 from base.basedriver import Basedriver
 from utilities.locators import Homepage_locators
 from pages.signup_login_page import Signup_Login_page
+from pages.contact_us_page import ContactUs_page
 
 
 class Homepage(Basedriver):
@@ -10,8 +11,7 @@ class Homepage(Basedriver):
         
     def homepage_isvisible(self):
         try:
-            self.wait_until_element_is_visible(self.navbar_home)
-            self.wait_until_element_is_visible(self.navbar_products)
+            self.wait_until_element_is_visible(self.locators.home_slider)
             return self.driver.title == 'Automation Exercise'
         except Exception:
             return False 
@@ -19,37 +19,9 @@ class Homepage(Basedriver):
     def click_signup_login_btn(self):
         self.go_to_signup_login_page()
         return Signup_Login_page(self.driver)
-        
-        
-        
-        
-        
-        
-        
     
-        
     def click_contact_us_btn(self):
-        self.click_on_element(self.locators.contact_us_btn)
-        
-    def click_test_cases_btn(self):
-        self.click_on_element(self.locators.test_cases_btn)
-        
-    def click_products_btn(self):
-        self.click_on_element(self.locators.products_btn)
-        
-    def click_cart_btn(self):
-        self.click_on_element(self.locators.cart_btn)
-        
-    def click_view_product_btn(self):
-        self.click_on_element(self.locators.view_product)
-        
-    def click_add_to_cart_btn(self):
-        self.click_on_element(self.locators.add_to_cart)
-    
-    def click_home_btn(self):
-        self.click_on_element(self.locators.home_btn)
-    
-    def click_logout_btn(self):
-        self.click_on_element(self.locators.logout_btn)
-        
+        self.go_to_contact_us_page()
+        return ContactUs_page(self.driver)   
+
     
