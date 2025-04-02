8856af05a77f48669faba2294958711e2c1e66f0
Pranjal Melkani <pranjal.melkani18@gmail.com>
2025-04-02 14:53:04 +0530

    updated user registration tests

--- tests/test_register_user.py
+++ tests/test_register_user.py
@@ -1,10 +1,12 @@
-import time
+from utilities.testdata import Testdata
 from pages.homepage import Homepage
 from faker import Faker
 
+import pytest
 
+@pytest.mark.skip
 class TestRegisterUser:
-    def test_registration(self, driver):
+    def test_new_registration(self, driver):
         fake = Faker()
         name = fake.name()
         email_id = fake.email()
@@ -23,6 +25,7 @@ class TestRegisterUser:
         mob_no = fake.phone_number()
         
         hp = Homepage(driver)
+        hp.open_url(Testdata.url)
         assert hp.homepage_isvisible(), \
             "Homepage is not visible. Some elements are missing, or webpage title is not 'Automation Exercise'"
         slp = hp.click_signup_login_btn()
@@ -36,12 +39,27 @@ class TestRegisterUser:
         assert slp.account_created_isvisible(), \
             "'ACCOUNT CREATED!' is not visible"
         slp.click_continue_btn()
-        logged_in_text = slp.get_logged_in_as_value()
+        logged_in_text = slp.get_logged_in_user_details()
         assert logged_in_text.__contains__(f"Logged in as {name}"), \
             f"'Logged in as {name}' is not visible"
         slp.click_delete_account_btn()
         assert slp.account_deleted_isvisible(), \
             "'ACCOUNT DELETED!' is not visible"
         slp.click_continue_btn()
-            
+    
+    
+    def test_duplicate_registration(self, driver):
+        name = "TEST ADMIN"
+        email_id = "testingemail_admin@gmail.com"
+        
+        hp = Homepage(driver)
+        hp.open_url(Testdata.url)
+        assert hp.homepage_isvisible(), \
+            "Homepage is not visible. Some elements are missing, or webpage title is not 'Automation Exercise'"
+        slp = hp.click_signup_login_btn()
+        assert slp.signup_field_isvisible(), \
+            "Signup field is not visible"
+        slp.signup_user(name, email_id)
+        assert slp.email_exists_error_isvisible(), \
+            "'Email Address already exist!' is not visible"
         
