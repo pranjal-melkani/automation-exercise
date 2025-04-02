3b71551596ec95645e808f8e1a52767a42669fb0
Pranjal Melkani <pranjal.melkani18@gmail.com>
2025-04-02 14:51:49 +0530

    updated homepage actions

--- pages/homepage.py
+++ pages/homepage.py
@@ -8,9 +8,25 @@ class Homepage(Basedriver):
         super().__init__(driver)
         self.locators = Homepage_locators
         
+    def homepage_isvisible(self):
+        try:
+            self.wait_until_element_is_visible(self.navbar_home)
+            self.wait_until_element_is_visible(self.navbar_products)
+            return self.driver.title == 'Automation Exercise'
+        except Exception:
+            return False 
+        
     def click_signup_login_btn(self):
-        self.click_on_element(self.locators.signup_login_btn)
+        self.go_to_signup_login_page()
         return Signup_Login_page(self.driver)
+        
+        
+        
+        
+        
+        
+        
+    
         
     def click_contact_us_btn(self):
         self.click_on_element(self.locators.contact_us_btn)
@@ -24,17 +40,16 @@ class Homepage(Basedriver):
     def click_cart_btn(self):
         self.click_on_element(self.locators.cart_btn)
         
-    def click_view_product(self):
+    def click_view_product_btn(self):
         self.click_on_element(self.locators.view_product)
         
-    def click_add_to_cart(self):
+    def click_add_to_cart_btn(self):
         self.click_on_element(self.locators.add_to_cart)
+    
+    def click_home_btn(self):
+        self.click_on_element(self.locators.home_btn)
+    
+    def click_logout_btn(self):
+        self.click_on_element(self.locators.logout_btn)
         
-    def homepage_isvisible(self):
-        try:
-            self.wait_until_element_is_visible(self.locators.signup_login_btn)
-            self.wait_until_element_is_visible(self.locators.cart_btn)
-            self.wait_until_element_is_visible(self.locators.add_to_cart)
-            return self.driver.title == 'Automation Exercise'
-        except Exception:
-            return False
+    
