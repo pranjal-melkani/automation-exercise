8f1b09d2bba15cd94a5393a133f3f15aa82995b2
Pranjal Melkani <pranjal.melkani18@gmail.com>
2025-04-02 14:51:17 +0530

    updated basedriver with nav controls

--- base/basedriver.py
+++ base/basedriver.py
@@ -2,9 +2,20 @@ from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.select import Select
 from utilities.testdata import Testdata
+from selenium.webdriver.common.by import By
 
 
 class Basedriver:
+    navbar_home = (By.XPATH, "//a[contains(text(), 'Home')]")
+    navbar_products = (By.XPATH, "//a[contains(text(), 'Products')]")
+    navbar_cart = (By.XPATH, "//a[contains(text(), 'Cart')]")
+    navbar_signup_login = (By.XPATH, "//a[contains(text(), 'Signup / Login')]")
+    navbar_testcases = (By.XPATH, "//a[contains(text(), 'Test Cases')]")
+    navbar_api_testing = (By.XPATH, "//a[contains(text(), 'API Testing')]")
+    navbar_contact_us = (By.XPATH, "//a[contains(text(), 'Contact us')]")
+    navbar_loggedinas_field = (By.CSS_SELECTOR, ".navbar-nav li:last-child")
+    navbar_logout = (By.XPATH, "//a[contains(text(), 'Logout')]")
+    
     def __init__(self, driver):
         self.driver = driver
         
@@ -24,3 +35,25 @@ class Basedriver:
     def select_dropdown_by_visible_text(self, locator, visible_text):
         element = self.wait_until_element_is_visible(locator)
         Select(element).select_by_visible_text(visible_text)
+        
+    def open_url(self, url):
+        self.driver.get(url)
+        
+    def go_to_homepage(self):
+        self.click_on_element(self.navbar_home)
+    
+    def open_products_page(self):
+        self.click_on_element(self.navbar_products)
+        
+    def open_cart(self):
+        self.click_on_element(self.navbar_cart)
+        
+    def go_to_signup_login_page(self):
+        self.click_on_element(self.navbar_signup_login)
+        
+    def get_logged_in_user_details(self):
+        return self.wait_until_element_is_visible(self.navbar_loggedinas_field).text
+    
+    def logout(self):
+        self.click_on_element(self.navbar_logout)
+        
